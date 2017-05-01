cf89b93d0be9499fd2d7d29f027432710b871db5
Kate Leslie <thisizk8@gmail.com>
2017-04-30 22:01:46 -0500

    js for modal window

--- app/index.js
+++ app/index.js
@@ -1,5 +1,25 @@
 import 'whatwg-fetch';
 
+const portfolioItems = [{
+  photo: "https://media1.giphy.com/media/m7BTtLWhjkEJa/200_s.gif",
+  text: "here's the thing"
+}, {
+  photo: "https://www.destructoid.com//ul/428292-grumpycat.jpg",
+  text: "here's the thing"
+}, {
+  photo: "https://lh3.googleusercontent.com/U4IsC9Ri4KTmf-eCW-C0G6o06bULQ3zNSgPH8EbWbPBxr-rN_JE0rY3v0f6WSPFw3eU=w300",
+  text: "here's the thing"
+}, {
+  photo: "https://rlv.zcache.com/svc/getimage?id=4b90b386-cb5f-4bd6-b68a-a930f7be270e&max_dim=324&square_it=true",
+  text: "here's the thing"
+}, {
+  photo: "https://i0.wp.com/100happybirthdaymemes.com/wp-content/uploads/2016/06/Grumpy-Cat-Memes-01.jpg",
+  text: "here's the thing"
+}, {
+  photo: "http://www.catster.com/wp-content/uploads/2015/08/grumpy-cat-meme-051.jpg",
+  text: "here's the thing"
+}];
+
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -10,10 +30,14 @@ var btns = document.getElementsByClassName("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-Array.from(btns).forEach((btn) => {
+Array.from(btns).forEach((btn, index) => {
   btn.onclick = function() {
     modal.style.display = "block";
+    const data = portfolioItems[index]
+    document.getElementById('photo').src = data.photo
+    document.getElementById('text').innerHTML = data.text
   }
+
 });
 
 
